5071b15d6619130af38aad048298095feff6f3b8
ChrisM345 <chrism.12277@gmail.com>
2024-03-13 18:51:22 -0500

    Background image. Home page created using Javascript. Buttons have event listeners now

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import divContent from './main-page.js'
+import Pancakes from './pancakes.jpg';
+import './style.css';
 console.log("testing")
 
+const bckground = document.querySelector('body');
+
+bckground.style.backgroundImage=`url(${Pancakes})`;
+
+const credit = document.createElement('p')
+credit.textContent = 'Image Credit - https://unsplash.com/photos/brown-cookies-on-brown-wooden-table-tk9dBabQde0'
+
+bckground.append(credit);
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        console.log("pressed")
+    })
+})
--- src/main-page.js
+++ src/main-page.js
@@ -1,3 +1,18 @@
+import Pancakes from './pancakes.jpg';
+
 const divContent = (() => {
-    console.log("testing main-page")
+    const content = document.querySelector("#content");
+    const header = document.createElement('h3')
+    header.textContent = 'Pancake Bee Cafe'
+
+    // const myImage = new Image();
+    // myImage.src = Pancakes
+    // myImage.className = 'background-image'
+
+    const intro = document.createElement('p')
+    intro.textContent = 'Welcome to Pancake Bee Cafe. We believe in simple ingredients, generous portions, and excellent customer service. Please come visit our cozy cafe and enjoy a lovely breakfast with us.'
+
+    content.append(header)
+    // content.append(myImage)
+    content.append(intro)
 })();
